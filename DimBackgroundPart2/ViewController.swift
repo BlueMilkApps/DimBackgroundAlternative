c641d360402df29b4ffcb9af568c8172c71ecd7f
Chris <support@totem.training>
2016-01-03 07:49:49 -0500

    Initial Commit

---
+++ DimBackgroundPart2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DimBackgroundPart2
+//
+//  Created by Chris Forant on 1/3/16.
+//  Copyright © 2016 Totem. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
